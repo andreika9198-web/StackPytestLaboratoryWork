dd65db3f34a0f3a2a024ed8580b22ce885e7df2d
Sapranov Andrey <andreika9198@gmail.com>
2026-04-16 10:00:45 +0300

    Создал структуру проекта и начал создавать класс Queue

---
+++ Class/my_class.py
@@ -0,0 +1,70 @@
+class Node:
+    def __init__(self,data , next_node = None):
+        self.data = data
+        self.next_node = next_node
+
+class Queue:
+    def __init__(self,max_length, head=None, tail=None):
+        self.head = head
+        self.tail = tail
+        self.max_length = max_length
+
+    def enqueue(self, data):
+        if self.size_stack() >= self.max_length:
+            print('Очередь переполнена')
+            return
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+        else:
+            self.tail.next_node = new_node
+        self.tail = new_node
+
+    def dequeue(self):
+        if not self.head:
+            return None
+        else:
+            dequeue_item = self.head
+            self.head = self.head.next_node
+            return dequeue_item.data
+
+    def size_stack(self):
+        counter = 0
+        stack_item = self.head
+        while stack_item:
+            counter += 1
+            stack_item = stack_item.next_node
+        return counter
+
+    def peek_first(self):
+        if not self.head:
+            print(f'Очередь пуста')
+        else:
+            print(self.head.data)
+
+    def peek_last(self):
+        if not self.head:
+            print(f'Очередь пуста')
+        else:
+            print(self.tail.data)
+
+    def show(self):
+        if not self.head:
+            print(f'Очередь пуста')
+        else:
+            current = self.head
+            while current:
+                print(current.data)
+                current = current.next_node
+
+    def is_full(self):
+        pass
+
+
+if __name__ == '__main__':
+    node = Node(1)
+    queue = Queue(3, node, node)
+    for i in range(2, 6):
+        queue.enqueue(i)
+    queue.show()
+

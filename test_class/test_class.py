8c01b5fd88216505d09623d34fe7fbf59a841ba0
Sapranov Andrey <andreika9198@gmail.com>
2026-04-20 13:59:39 +0300

    Добавил комментария к тестам

--- test_class/test_class.py
+++ test_class/test_class.py
@@ -1,28 +1,52 @@
 import pytest
 
 def test_01_01_enqueue(queue_obj_full):
+    """
+    Тест для проверки добавление в полный стек
+    """
     assert queue_obj_full.enqueue(11) == False
 
 def test_01_02_enqueue(queue_obj):
+    """
+    Тест для проверки добавление в пустой стек
+    """
     assert queue_obj.enqueue(11) == True
     assert queue_obj.head.data == 11
 
 def test_01_03_enqueue(queue_obj_filled):
+    """
+    Тест для проверки добавление в не пустой стек
+    """
     assert queue_obj_filled.enqueue(5) == True
     assert queue_obj_filled.head.data == 0
 
 def test_02_01_dequeue(queue_obj):
+    """
+    Тест для проверки удаления данных из пустого стека
+    """
     assert queue_obj.dequeue() == False
 
 def test_02_02_dequeue(queue_obj_filled):
+    """
+    Тест для проверки удаления данных из стека
+    """
     assert queue_obj_filled.dequeue() == 0
 
 def test_03_01_size(queue_obj_filled):
+    """
+    Тест для проверки подсчета количества элементов в стеке
+    """
     assert  queue_obj_filled.size() == 5
 
 def test_04_01_peek_first(queue_obj):
+    """
+    Тест для проверки отображения первого элемента из пустого стека
+    """
     assert queue_obj.peek_first() == False
 
 def test_04_02_peek_first(queue_obj_filled):
+    """
+    Тест для проверки отображения первого элемента из пустого стека
+    """
     assert queue_obj_filled.peek_first() == True
     assert queue_obj_filled.head.data == 0
